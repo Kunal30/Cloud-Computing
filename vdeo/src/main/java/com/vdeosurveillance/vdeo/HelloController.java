ce1005df8fdcb886b9785af8b3789c41dcd206c6
Kunal30 <kunalvsuthar@gmail.com>
2019-03-06 01:31:40 -0700

    Added initial  SpringBoot WebApp on localhost:8080

---
+++ vdeo/src/main/java/com/vdeosurveillance/vdeo/HelloController.java
@@ -0,0 +1,14 @@
+package com.vdeosurveillance.vdeo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Kaise Ho???? Khaana Kha Ke Jaaana Ming Zhao!!!";
+    }
+
+}
---
+++ vdeo/src/test/java/com/vdeosurveillance/vdeo/VdeoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vdeosurveillance.vdeo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VdeoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
