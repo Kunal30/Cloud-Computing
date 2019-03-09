3304977de74a40297488761061c232027a59c9e2
Kunal30 <kunalvsuthar@gmail.com>
2019-03-09 00:50:50 -0700

    Added SQS input messaging from Web Tier Instance

---
+++ Web-Tier-Instance/src/main/java/com/amazonaws/samples/WebTierInitiation.java
@@ -0,0 +1,71 @@
+package com.amazonaws.samples;
+import java.io.*;
+import java.util.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.vdeosurveillance.vdeo.VdeoApplication;
+
+public class WebTierInitiation {
+
+	public static void main(String args[])throws IOException
+	{
+		SpringApplication.run(VdeoApplication.class, args);
+		System.out.println("Heeeyyyyaaaa!!!");
+		scaleOut();
+		
+	}
+	public static void scaleOut()
+	{
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/home/kunal/.aws/credentials), and is in valid format.",
+                    e);
+        }
+		
+		// creating an SQS object to send input trigger to the queue for video surveillance
+		AmazonSQS sqs = AmazonSQSClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion(Regions.US_WEST_1)
+                .build();
+		
+		List<String> queueURLs= sqs.listQueues().getQueueUrls();
+		String queueURL= queueURLs.get(0);
+		System.out.println(queueURL);
+		
+		ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueURL);
+
+		while(true)
+	    {
+//			System.out.println("Helllloo Worlrlrlrlrlrd");
+		}        
+		
+
+	}
+	@RequestMapping("/")
+	public String index() {
+	    	System.out.println("Heyaaaaaa2");
+	       return "Hello Kaise Ho???? Khaana Kha Ke Jaaana Ming Zhao!!!";
+	    }
+}
